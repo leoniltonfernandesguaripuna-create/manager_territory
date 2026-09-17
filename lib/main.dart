77b63fca869cffcb577a9cd4254bdb4a43f8614f
leoniltonfernandesguaripuna-create <leoniltonfernandesguaripuna@gmail.com>
2026-09-16 21:47:52 -0300

    Initialize Flutter app with main structure

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'screens/home_page.dart';
+
+void main() => runApp(const TerritoryManagerApp());
+
+class TerritoryManagerApp extends StatelessWidget {
+  const TerritoryManagerApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Territory Manager',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: const Color(0xFFF0F2F5),
+        fontFamily: 'Roboto',
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xFF1A365D),
+          brightness: Brightness.light,
+        ),
+      ),
+      home: const HomePage(),
+    );
+  }
+}
