d2d6c2b48c85af312165cf318ea0fc1281c4e4e3
leoniltonfernandesguaripuna-create <leoniltonfernandesguaripuna@gmail.com>
2026-09-18 23:59:22 -0300

    Implement Cloud class for Firestore operations

---
+++ cloud.dart
@@ -0,0 +1,35 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+class Cloud {
+  static final FirebaseFirestore _db = FirebaseFirestore.instance;
+  static bool _pronto = false;
+
+  static Future<void> iniciar() async {
+    try {
+      await Firebase.initializeApp();
+      _pronto = true;
+    } catch (_) {
+      _pronto = false;
+    }
+  }
+
+  static bool get disponivel => _pronto;
+
+  static Future<void> salvar(String doc, Map<String, dynamic> dados) async {
+    if (!_pronto) return;
+    try {
+      await _db.collection('congregacao').doc(doc).set(dados);
+    } catch (_) {}
+  }
+
+  static Future<Map<String, dynamic>?> ler(String doc) async {
+    if (!_pronto) return null;
+    try {
+      final d = await _db.collection('congregacao').doc(doc).get();
+      return d.data();
+    } catch (_) {
+      return null;
+    }
+  }
+}
