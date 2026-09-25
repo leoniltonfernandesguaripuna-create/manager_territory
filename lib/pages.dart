aefbd715b620c846cecd8f64f4dc8a7d3975aff7
leoniltonfernandesguaripuna-create <leoniltonfernandesguaripuna@gmail.com>
2026-09-25 00:06:02 -0300

    Add initial pages.dart file with imports

---
+++ lib/pages.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'package:image_picker/image_picker.dart';
+import 'cloud.dart';
+import 'tema.dart';
+import 'stores.dart';
+import 'widgets.dart';
