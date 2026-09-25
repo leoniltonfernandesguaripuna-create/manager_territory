8cae5b99f60e0590f48bd8195fa2dfb2cab6708e
leoniltonfernandesguaripuna-create <leoniltonfernandesguaripuna@gmail.com>
2026-09-24 22:41:59 -0300

    Remove color constants from class C
    Removed unused color constants from class C.

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'dart:convert';
 import 'package:image_picker/image_picker.dart';
 import 'cloud.dart';
+import 'tema.dart';                    
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -80,21 +81,6 @@ class TerritorioApp extends StatelessWidget {
   }
 }
 
-class C {
-  static const azul = Color(0xFF1A365D);
-  static const azulMedio = Color(0xFF2C4A73);
-  static const bege = Color(0xFFF3EAD3);
-  static const cinza = Color(0xFFA0AEC0);
-  static const cinzaClaro = Color(0xFFF0F2F5);
-  static const amarelo = Color(0xFFD4A437);
-  static const amareloClaro = Color(0xFFFFF3C4);
-  static const borda = Color(0xFFD1D5DB);
-  static const cinzaForm = Color(0xFFDDDDDD);
-  static const verde = Color(0xFF2F855A);
-  static const vermelho = Color(0xFFC53030);
-  static const vinho = Color(0xFF800020);
-  static const cinzaSabado = Color(0xFFD3D3D3);
-}
 
 // ============== AUTH STORE ==============
 class AuthStore extends ChangeNotifier {
